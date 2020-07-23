b6887e2f49a6ec1cf4ac5f73dddd90fbad37bffd
sakibhossen202 <67454665+sakibhossen202@users.noreply.github.com>
2020-07-23 18:29:55 +0600

    Mid-level complete

---
+++ script.js
@@ -0,0 +1,60 @@
+//submit-btn handler
+const submit = document.getElementById('submit-btn');
+submit.addEventListener('click',function(){
+    const submitAct = document.getElementById('login-area');
+    submitAct.style.display = "none"
+    const transaction = document.getElementById('transaction-area');
+    transaction.style.display = "block"
+
+})
+
+// deposit button event handler
+
+const depositvalue = document.getElementById('depositAmount');
+    depositvalue.addEventListener('click',function(){
+   
+    const depositNumber = getInput("depositCount");
+
+    
+
+    
+
+    updateTwoAmount("currentDeposit", depositNumber);
+
+    document.getElementById('depositCount').value = "";
+
+    updateTwoAmount("currentBalance", depositNumber);
+    
+
+    
+    // updateAmount("afterWithdraw", total);
+
+    
+})  
+
+function updateTwoAmount(id, depositNumber){
+    const current= document.getElementById(id).innerText;
+    const presentBalance = parseFloat(current);
+    const total = presentBalance + depositNumber;
+    document.getElementById(id).innerText = total;
+}
+
+//withdraw button event handler
+document.getElementById("withdrawBtn").addEventListener("click", function(){
+   const withdrawCount = getInput("withdrawInput");
+   console.log(withdrawCount);
+
+    
+
+})
+function getInput(id){
+    const Value = document.getElementById(id).value;
+    const amountNumber = parseFloat(Value);
+    return amountNumber;
+}
+
+
+
+
+    
+
